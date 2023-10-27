45f26469317cfe3ed0d42034b685c6d7ac09c87c
prathyushamanjula <prathyumanjula@gmail.com>
2023-10-27 16:05:20 +0530

    Project Created

---
+++ src/test/java/org/jsp/JobPortal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.JobPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
